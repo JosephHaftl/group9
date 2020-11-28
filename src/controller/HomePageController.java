c3a24466635c16c590230d04478b210a0a269c45
ianchong16 <ianchong16@192.168.1.110>
2020-11-28 11:19:26 -0500

    Created controller and fixed errors

---
+++ src/controller/HomePageController.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+/**
+ *
+ * @author ianchong16
+ */
+
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class HomePageController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    void createPost(ActionEvent event) {
+
+    }
+
+    @FXML
+    void deletePost(ActionEvent event) {
+
+    }
+
+    @FXML
+    void updatePost(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+
+    }
+}
+
+
+
+
