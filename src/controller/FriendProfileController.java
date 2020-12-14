12ff152c80243e51a0cc6019c0f780cbb843cfe9
nicole cooney <njc5496@psu.edu>
2020-12-14 11:13:29 -0500

    friend profile working

--- src/controller/FriendProfileController.java
+++ src/controller/FriendProfileController.java
@@ -7,11 +7,15 @@ package controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
 import model.Friendmodel;
 
 /**
@@ -19,27 +23,51 @@ import model.Friendmodel;
  * @author nicole
  */
 public class FriendProfileController {
-    
-    
+
     @FXML
     private ResourceBundle resources;
 
     @FXML
     private URL location;
 
-
     @FXML // fx:id="profilePic"
     private ImageView profilePic; // Value injected by FXMLLoader
 
     @FXML // fx:id="name"
     private Label name; // Value injected by FXMLLoader
+    
+//    @FXML
+//    private Button backButton;
+
+    @FXML
+    private Label notes;
 
     Friendmodel selectedModel;
     Scene previousScene;
+    
+//    
+//    @FXML
+//    void backButton(ActionEvent event) {
+//          Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+//        
+//        //  option 2: get current stage -- from backbutton        
+//        // Stage stage = (Stage)backButton.getScene().getWindow();
+//        
+//        if (previousScene != null) {
+//            stage.setScene(previousScene);
+//        }
+//
+//    }
+//       public void setPreviousScene(Scene scene) {
+//        previousScene = scene;
+//        backButton.setDisable(false);
+//
+//    }
 
     public void initData(Friendmodel model) {
         selectedModel = model;
         name.setText(model.getName());
+        notes.setText(model.getNotes());
 
         try {
             // path points to /resource/images/
@@ -51,12 +79,12 @@ public class FriendProfileController {
             System.out.println(ex.getMessage());
         }
     }
-    
+
     @FXML
     void initialize() {
         assert profilePic != null : "fx:id=\"profilePic\" was not injected: check your FXML file 'FriendsProfilePage.fxml'.";
         assert name != null : "fx:id=\"name\" was not injected: check your FXML file 'FriendsProfilePage.fxml'.";
 
     }
-    
+
 }
