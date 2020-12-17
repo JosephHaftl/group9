3dd9da7fa7150d53098ca89a3b7dfc1d222ba695
nicole cooney <njc5496@psu.edu>
2020-12-16 19:39:40 -0500

    test cases

---
+++ test/controller/HomePageControllerTest.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author nicole
+ */
+public class HomePageControllerTest {
+    
+    public HomePageControllerTest() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+    }
+
+    /**
+     * Test of setTableData method, of class HomePageController.
+     */
+    @Test
+    public void testSetTableData() {
+    }
+
+    /**
+     * Test of initData method, of class HomePageController.
+     */
+    @Test
+    public void testInitData() {
+    }
+
+    /**
+     * Test of actionShowMessages method, of class HomePageController.
+     */
+    @Test
+    public void testActionShowMessages() throws Exception {
+    }
+
+    /**
+     * Test of initialize method, of class HomePageController.
+     */
+    @Test
+    public void testInitialize() {
+    }
+
+    /**
+     * Test of create method, of class HomePageController.
+     */
+    @Test
+    public void testCreate() {
+    }
+
+    /**
+     * Test of readAll method, of class HomePageController.
+     */
+    @Test
+    public void testReadAll() {
+    }
+
+    /**
+     * Test of update method, of class HomePageController.
+     */
+    @Test
+    public void testUpdate() {
+    }
+
+    /**
+     * Test of delete method, of class HomePageController.
+     */
+    @Test
+    public void testDelete() {
+    }
+
+    /**
+     * Test of readById method, of class HomePageController.
+     */
+    @Test
+    public void testReadById() {
+    }
+    
+}
